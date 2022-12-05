e17c7c432b036f79a7a42e7cb26bae2aaf06a6c5
Галактиоши <galaktionov-19911@mail.ru>
2022-12-05 17:05:23 +0500

    Work is done!

--- app.py
+++ app.py
@@ -5,8 +5,25 @@ candidates = load_candidates_from_json('candidates.json')
 
 app = Flask(__name__)
 
+
 @app.route("/")
 def page_index():
-  return get_all(response_json)
+    return get_all_candidates(candidates)
+
+
+@app.route("/candidate/<int:uid>")
+def page_candidates(uid):
+    return get_candidate(uid, candidates)
+
+
+@app.route("/search/<uid>")
+def page_search(uid):
+    return get_candidates_by_name(uid, candidates)
+
+
+@app.route("/skill/<uid>")
+def page_skill(uid):
+    return get_by_skill(uid, candidates)
+
 
 app.run()
--- utils.py
+++ utils.py
@@ -3,10 +3,21 @@ import json
 
 def load_candidates_from_json(data):
     """
-    Список всех кандидатов
+    Загрузка кандидатов
     """
     with open(data, 'r', encoding='utf-8') as file:
         return json.load(file)
+
+
+def get_all_candidates(data):
+    """
+    Список всех.
+    """
+    result = '<h1>Все кандидаты</h1><br />'
+    for name in data:
+        result += f"""<p><a href="http://127.0.0.1:5000/candidate/{name['id']}">{name['name']}</a></p>
+                  """
+    return result
 
 
 def get_candidate(id, data):
@@ -23,16 +34,15 @@ def get_candidate(id, data):
                        """
     return result
 
-# Остановился здесь!
+
 def get_candidates_by_name(name_candidate, data):
     """
     Одного кандидата по имени
     """
-    result = '<h1>Все кандидаты</h1><br />'
+    result = '<h1>Найдено кандидатов:</h1><br />'
     for name in data:
-        if name_candidate == name['name']:
-        # Вернуться к ссылке!
-            result += f"""<p><a href="/candidate/<{name['id']}>">{name['name']}</a></p><br />
+        if name_candidate.lower() in name['name'].lower():
+            result += f"""<p><a href="http://127.0.0.1:5000/candidate/{name['id']}">{name['name']}</a></p><br />
                        """
     return result
 
@@ -44,12 +54,13 @@ def get_by_skill(skill_name, data):
     sum_candidates = []
     result = f"""<h1>Найдено со скиллом {skill_name}: """
     for name in data:
-        if skill_name.lower() in name['skills']:
+        if skill_name.lower() in name['skills'].lower():
             sum_candidates.append(name['name'])
 
     result += f"""{len(sum_candidates)}</h1><br />"""
 
     for name in data:
-        result += f"""<p><a href="/candidate/<{name['id']}>">{name['name']}</a></p><br />"""
+        if skill_name.lower() in name['skills'].lower():
+            result += f"""<p><a href="http://127.0.0.1:5000/candidate/{name['id']}">{name['name']}</a></p><br />"""
 
     return result
