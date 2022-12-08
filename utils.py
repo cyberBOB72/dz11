67a1505cac6262e743c2af5133600c3730eac5b6
Artem <118754148+cyberBOB72@users.noreply.github.com>
2022-12-08 22:17:20 +0500

    Update utils.py
    Change all utils for template.

--- utils.py
+++ utils.py
@@ -13,37 +13,27 @@ def get_all_candidates(data):
     """
     Список всех.
     """
-    result = '<h1>Все кандидаты</h1><br />'
-    for name in data:
-        result += f"""<p><a href="http://127.0.0.1:5000/candidate/{name['id']}">{name['name']}</a></p>
-                  """
-    return result
+    return load_candidates_from_json(data)
 
 
 def get_candidate(id, data):
     """
     Одного кандидата по его id.
     """
-    result = '<h1>'
     for name in data:
         if id == name['id']:
-            result += f"""{name['name']}</h1><br />
-                       <p>{name['position']}</p><br />
-                       <img scr="{name['picture']}" width=200/><br />
-                       <p>{name['skills']}</p>
-                       """
-    return result
+            return name
+    return 'Not Found'
 
 
 def get_candidates_by_name(name_candidate, data):
     """
     Одного кандидата по имени.
     """
-    result = '<h1>Найдено кандидатов:</h1><br />'
+    result = []
     for name in data:
         if name_candidate.lower() in name['name'].lower():
-            result += f"""<p><a href="http://127.0.0.1:5000/candidate/{name['id']}">{name['name']}</a></p><br />
-                       """
+            result.append(name)
     return result
 
 
@@ -51,16 +41,9 @@ def get_by_skill(skill_name, data):
     """
     Вернет кандидатов по навыку.
     """
-    sum_candidates = []
-    result = f"""<h1>Найдено со скиллом {skill_name}: """
+    result = []
     for name in data:
-        if skill_name.lower() in name['skills'].lower():
-            sum_candidates.append(name['name'])
-
-    result += f"""{len(sum_candidates)}</h1><br />"""
-
-    for name in data:
-        if skill_name.lower() in name['skills'].lower():
-            result += f"""<p><a href="http://127.0.0.1:5000/candidate/{name['id']}">{name['name']}</a></p><br />"""
-
-    return result
+        skills = name['skills'].lower().split(', ')
+        if skill_name.lower() in skills:
+            result.append(name)
+    return result 
