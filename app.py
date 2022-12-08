8445f6446eb29538c717a13061fe3809618298e7
Artem <118754148+cyberBOB72@users.noreply.github.com>
2022-12-08 23:32:15 +0500

    Update app.py
    Update work for use templates.

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 from utils import *
-from flask import Flask
+from flask import Flask, render_template
 
 candidates = load_candidates_from_json('candidates.json')
 
@@ -8,22 +8,26 @@ app = Flask(__name__)
 
 @app.route("/")
 def page_index():
-    return get_all_candidates(candidates)
+    candidates = get_all_candidates(candidates)
+    return render_template('list.html', candidates=candidates)
 
 
 @app.route("/candidate/<int:uid>")
-def page_candidates(uid):
-    return get_candidate(uid, candidates)
+def page_candidate(uid):
+    candidate = get_candidate(uid, candidates)
+    return render_template('card.html', candidate=candidate)
 
 
 @app.route("/search/<uid>")
 def page_search(uid):
-    return get_candidates_by_name(uid, candidates)
+    candidates = get_candidates_by_name(uid, candidates)
+    return render_template('search.html', candidates=candidates, count=len(candidates))
 
 
 @app.route("/skill/<uid>")
 def page_skill(uid):
-    return get_by_skill(uid, candidates)
+    candidates = get_by_skill(uid, candidates)
+    return render_template('skill.html', candidates=candidates, count=len(candidates))
 
 
 app.run()
